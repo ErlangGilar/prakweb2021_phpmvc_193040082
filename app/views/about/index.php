7ee25f08cfbf4785f28b400a0022a23e833dbc83
ErlangGilar <ErlangGilar@users.noreply.github.com>
2021-10-01 17:01:16 +0700

    Mengikuti video 5-view, Membuat view untuk aplikasi mvc lalu membuat template html dan menginstansiasikannya

---
+++ app/views/about/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Halo nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?>, saya adalah seorang <?= $data['pekerjaan'];?></p>
+</body>
+</html>
